c0fdbacd8fe1745b9dff68c4bffe25e57cbecf0f
migayalach <ayalachavezmiguel@gmail.com>
2025-02-16 00:45:49 -0400

    es una prueba

--- src/app/video/page.tsx
+++ src/app/video/page.tsx
@@ -3,11 +3,11 @@ import React, { useEffect } from "react";
 import { useAppSelector, useAppDispatch } from "../redux/hooks";
 import { getVideo } from "../redux/features/videoSlice";
 import { RootState } from "../redux/store";
+import CardVideo from "@/components/videos/card/CardVideo";
 
 function Page() {
   const dispatch = useAppDispatch();
   const selectVideo = useAppSelector((state: RootState) => state.videos);
-  console.log(selectVideo);
 
   useEffect(() => {
     dispatch(getVideo());
@@ -16,6 +16,7 @@ function Page() {
   return (
     <div>
       <h1>Videos</h1>
+      <CardVideo />
     </div>
   );
 }
