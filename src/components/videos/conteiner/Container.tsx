d89fb612c4c88a9b40a39ebb00c6b2e8f4be8265
migayalach <ayalachavezmiguel@gmail.com>
2025-02-18 23:37:07 -0400

    created

--- src/components/videos/conteiner/Container.tsx
+++ src/components/videos/conteiner/Container.tsx
@@ -2,25 +2,37 @@ import Date from "@/components/text/date/Date";
 import Description from "@/components/text/description/Description";
 import Title from "@/components/text/title/Title";
 import React from "react";
+import Multimedia from "../multimedia/Multimedia";
+import Ranking from "@/components/text/ranking/Ranking";
+import Like from "@/components/buttons/like/Like";
+import Dislike from "@/components/buttons/dislike/Dislike";
+import Download from "@/components/buttons/download/Download";
+import Suscription from "@/components/buttons/suscription/Suscription";
 
 function Container({
+  url,
   title,
   date,
   description,
+  averange,
 }: {
+  url: string;
   title: string;
   date: string;
   description: string;
+  averange: number;
 }) {
   return (
     <div>
-      video
-      <div>
-        <Title title={title} />
-        <Date date={date} />
-      </div>
+      <Multimedia url={url} />
+      <Title title={title} />
+      <Like />
+      <Dislike />
+      <Download />
+      <Suscription />
+      <Date date={date} />
       <Description description={description} />
-      averange
+      <Ranking averange={averange} />
     </div>
   );
 }
