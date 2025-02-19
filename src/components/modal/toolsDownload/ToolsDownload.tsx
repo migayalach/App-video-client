c2fe77933eb850e416c7fd75d4edb53393c45c9a
migayalach <ayalachavezmiguel@gmail.com>
2025-02-18 23:36:51 -0400

    crated

--- src/components/modal/toolsDownload/ToolsDownload.tsx
+++ src/components/modal/toolsDownload/ToolsDownload.tsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Button, Modal } from "antd";
+import { CloudDownloadOutlined } from "@ant-design/icons";
+import CollapseOpt from "@/components/videos/collapseOpt/CollapseOpt";
 
-function ToolsDownload() {
-  return (
-    <div>ToolsDownload</div>
-  )
+interface ToolsDownloadProps {
+  open: boolean;
 }
 
-export default ToolsDownload
+const ToolsDownload: React.FC = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleOk = () => {
+    setIsModalOpen(false);
+  };
+
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
+
+  return (
+    <>
+      <CloudDownloadOutlined onClick={showModal} />
+      <Modal
+        title="Download"
+        open={isModalOpen}
+        onOk={handleOk}
+        onCancel={handleCancel}
+      >
+        <CollapseOpt />
+      </Modal>
+    </>
+  );
+};
+
+export default ToolsDownload;
