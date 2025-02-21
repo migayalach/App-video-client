af1dbbc488b04b2c55c640e456088123cc050b90
migayalach <ayalachavezmiguel@gmail.com>
2025-02-21 10:55:48 -0400

    entry point ready

--- src/app/download/page.tsx
+++ src/app/download/page.tsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import Lower from "@/component/lower/Lower";
+import React from "react";
 
 function page() {
   return (
-    <div>Download</div>
-  )
+    <div>
+      <h3>
+        Hello world, this is the section where you can download stuff from
+        Youtube. We will soon enable more platforms.
+      </h3>
+      <h4>What video or music do you need to download? </h4>
+      <p>Please enter or paste the URL!</p>
+      <Lower/>
+    </div>
+  );
 }
 
-export default page
+export default page;
