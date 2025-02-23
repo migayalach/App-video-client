d767913c31a4c862ead3937b844179f89e90af37
migayalach <ayalachavezmiguel@gmail.com>
2025-02-22 23:58:11 -0400

    src/app/video/page.tsx

--- src/app/video/page.tsx
+++ src/app/video/page.tsx
@@ -3,7 +3,7 @@ import React, { useEffect } from "react";
 import { useAppSelector, useAppDispatch } from "../../redux/hooks";
 import { getVideo } from "../../redux/features/videoSlice";
 import { RootState } from "../../redux/store";
-// import CardVideo from "@/components/videos/card/CardVideo";
+import CardVideo from "@/components/card/CardVideo";
 
 function Page() {
   const dispatch = useAppDispatch();
@@ -15,7 +15,7 @@ function Page() {
 
   return (
     <div>
-      {/* <CardVideo videos={selectVideo} /> */}
+      <CardVideo videos={selectVideo} />
     </div>
   );
 }
