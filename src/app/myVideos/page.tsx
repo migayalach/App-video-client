5014dc50c9918f228f04ee52881b55bfb260aa49
migayalach <ayalachavezmiguel@gmail.com>
2025-02-22 23:56:41 -0400

    src/app/myVideos/

---
+++ src/app/myVideos/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function page() {
+  return (
+    <div>
+      <h1>Hello user, here is your list of videos</h1>
+      <p>What do I need?</p>
+      <p><span>PARAMS:</span>idUser</p>
+      
+    </div>
+  );
+}
+
+export default page;
