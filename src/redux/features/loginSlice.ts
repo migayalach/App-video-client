e1dce44f445f590c3a04758e91470182ae38fbaf
migayalach <ayalachavezmiguel@gmail.com>
2025-02-24 23:11:55 -0400

    added new property 'idUser' at interfacce

--- src/redux/features/loginSlice.ts
+++ src/redux/features/loginSlice.ts
@@ -2,6 +2,7 @@ import { createSlice, createAsyncThunk, PayloadAction } from "@reduxjs/toolkit";
 import axios from "axios";
 
 export interface Login {
+  idUser: string;
   email: string;
   password: string;
 }
