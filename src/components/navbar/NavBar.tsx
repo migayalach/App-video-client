9611dce9efcc70745370305732ebaa04a3d7fd57
migayalach <ayalachavezmiguel@gmail.com>
2025-02-24 23:11:15 -0400

    update info

--- src/components/navbar/NavBar.tsx
+++ src/components/navbar/NavBar.tsx
@@ -37,7 +37,7 @@ const NavBar: React.FC = () => {
       key: "download",
       icon: <DownloadOutlined />,
     },
-    ...(userData
+    ...(!userData
       ? [
           {
             label: <Sign />,
