37b9fe7ea5161ec031f982009ef7c7976907869e
migayalach <ayalachavezmiguel@gmail.com>
2025-02-25 23:15:03 -0400

    in process

--- src/redux/features/favoriteSlice.ts
+++ src/redux/features/favoriteSlice.ts
@@ -11,7 +11,7 @@ const initialState: LikeState = {
 };
 
 export const getFavorites = createAsyncThunk<Results[], string>(
-  "like",
+  "like/getFavorites",
   async (idUser, { rejectWithValue }) => {
     try {
       const { data } = await axios.get<Results[]>(
@@ -31,25 +31,79 @@ export const getFavorites = createAsyncThunk<Results[], string>(
   }
 );
 
-export const postFavorite = () => {};
+export const postFavorite = createAsyncThunk<any, Partial<any>>(
+  "like/postFavorite",
+  async (information, { rejectWithValue }) => {
+    try {
+      const data = await axios.post<any>(
+        `${process.env.NEXT_PUBLIC_SERVER_URL}/like`,
+        information,
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      return data;
+    } catch (error: any) {
+      return rejectWithValue(
+        error.response?.data?.message || "Error al iniciar sesión"
+      );
+    }
+  }
+);
 
-export const deleteFavorite = () => {};
+export const deleteFavorite = createAsyncThunk<any, Partial<string>>(
+  "like/deleteFavorite",
+  async (information, { rejectWithValue }) => {
+    try {
+      const data = await axios.delete<any>(
+        `${process.env.NEXT_PUBLIC_SERVER_URL}/like/${information}`,
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      return data;
+    } catch (error: any) {
+      return rejectWithValue(
+        error.response?.data?.message || "Error al iniciar sesión"
+      );
+    }
+  }
+);
 
 const favoriteSlice = createSlice({
   name: "favorites",
   initialState,
   reducers: {},
   extraReducers: (builder) => {
-    //GET FAVORITES
-    builder.addCase(
-      getFavorites.fulfilled,
-      (state, action: PayloadAction<any>) => {
+    builder
+      // GET FAVORITES
+      .addCase(getFavorites.fulfilled, (state, action: PayloadAction<any>) => {
         state.info = action.payload.info;
         state.favorites = action.payload.results;
         state.error = null;
         state.loading = false;
-      }
-    );
+      })
+
+      // ADD NEW FAVORITE
+      .addCase(postFavorite.fulfilled, (state, action: PayloadAction<any>) => {
+        state.favorites = action.payload.video;
+        state.error = null;
+        state.loading = false;
+      })
+
+      // DELETE FAVORITE
+      .addCase(
+        deleteFavorite.fulfilled,
+        (state, action: PayloadAction<any>) => {
+          state.favorites = action.payload.video;
+          state.error = null;
+          state.loading = false;
+        }
+      );
   },
 });
 
