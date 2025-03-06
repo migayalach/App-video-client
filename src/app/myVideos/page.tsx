b84fb145c8e6f41d25f34b62be7778dab8b237ec
migayalach <ayalachavezmiguel@gmail.com>
2025-03-05 23:43:46 -0400

    my videos page ready

--- src/app/myVideos/page.tsx
+++ src/app/myVideos/page.tsx
@@ -1,12 +1,41 @@
-import React from "react";
+"use client";
+import React, { useEffect } from "react";
+import { useAppDispatch, useAppSelector } from "@redux/hooks";
+import { getAllList } from "@/redux/features/myVideos.slice";
+import CardVideo from "@/components/card/CardVideo";
+import FloatVideo from "@/components/floatButton/FloatVideo";
 
 function page() {
+  const dispatch = useAppDispatch();
+  const selectUser = useAppSelector(({ sign }) => sign?.user);
+  const myVideos = useAppSelector(({ myVideos }) => myVideos);
+
+  useEffect(() => {
+    if (selectUser) {
+      dispatch(getAllList(selectUser?.idUser));
+    }
+  }, []);
+
   return (
     <div>
       <h1>Hello user, here is your list of videos</h1>
-      <p>What do I need?</p>
-      <p><span>PARAMS:</span>idUser</p>
-       
+      <div>
+        {myVideos.results.map(({ idVideo, nameVideo, image, average }) => {
+          return (
+            <div key={idVideo}>
+              <CardVideo
+                idVideo={idVideo}
+                nameVideo={nameVideo}
+                image={image}
+                average={average}
+              />
+            </div>
+          );
+        })}
+      </div>
+      <div>
+        <FloatVideo />
+      </div>
     </div>
   );
 }
