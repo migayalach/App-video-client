eea17e52c36bdb2fae9c451018f7870b5c6c7c73
migayalach <ayalachavezmiguel@gmail.com>
2025-03-06 23:45:05 -0400

    component readu

--- src/components/form/form-video/FormVideo.tsx
+++ src/components/form/form-video/FormVideo.tsx
@@ -1,21 +1,82 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import type { FormProps } from "antd";
-import { Button, Checkbox, Form, Input } from "antd";
+import { Button, Form, Input, Switch } from "antd";
+import Cloudinary from "@/utils/Cloudinary";
+import TextArea from "antd/es/input/TextArea";
+import { useAppSelector, useAppDispatch } from "@/redux/hooks";
+import { postVideo } from "@/redux/features/myVideos.slice";
 
 type FieldType = {
-  username?: string;
-  password?: string;
-  remember?: string;
+  nameVideo: string;
+  description?: string;
+  url: string;
+  stateVideo?: boolean;
 };
 
-const onFinish: FormProps<FieldType>["onFinish"] = (values) => {
-  console.log("Success:", values);
-};
+interface dataInput {
+  option: string;
+}
 
-const FormVideo: React.FC = () => {
+const FormVideo: React.FC<dataInput> = ({ option }) => {
+  const dispatch = useAppDispatch();
+  const infoUser = useAppSelector(({ sign }) => sign?.user);
+  const information = useAppSelector(({ videos }) => videos?.videoDetail);
+
+  const [data, setData] = useState({
+    idUser: "",
+    nameVideo: "",
+    image: "",
+    description: "",
+    url: "",
+    stateVideo: true,
+  });
+
+  const handleChange = (event: { target: { name: string; value: string } }) => {
+    setData({
+      ...data,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const handleURLChange = (URL: string) => {
+    setData({
+      ...data,
+      image: URL,
+    });
+  };
+
+  const onChangeState = (checked: boolean) => {
+    setData({
+      ...data,
+      stateVideo: checked,
+    });
+  };
+
+  const onFinish: FormProps<FieldType>["onFinish"] = () => {
+    if (infoUser) {
+      if (!option) {
+        dispatch(postVideo({ ...data, idUser: infoUser?.idUser }));
+      } else if (option === "edit") {
+        console.log("editar");
+      }
+    }
+  };
+
+  useEffect(() => {
+    if (option === "edit" && information) {
+      setData({
+        idUser: information?.idUser,
+        nameVideo: information?.nameVideo,
+        image: information?.image,
+        description: information?.description,
+        url: information?.url,
+        stateVideo: information?.stateVideo ? true : false,
+      });
+    }
+  }, [option]);
+
   return (
     <Form
-      name="basic"
       labelCol={{ span: 8 }}
       wrapperCol={{ span: 16 }}
       style={{ maxWidth: 600 }}
@@ -24,30 +85,58 @@ const FormVideo: React.FC = () => {
       autoComplete="off"
     >
       <Form.Item<FieldType>
-        label="Username"
-        name="username"
-        rules={[{ required: true, message: "Please input your username!" }]}
+        label="Nane"
+        rules={[{ required: true, message: "Please input the name to video!" }]}
       >
-        <Input />
+        <Input
+          name="nameVideo"
+          onChange={handleChange}
+          value={data.nameVideo}
+        />
+      </Form.Item>
+
+      <Form.Item label="Image" valuePropName="fileList">
+        <Cloudinary onChange={handleURLChange} />
       </Form.Item>
 
       <Form.Item<FieldType>
-        label="Password"
-        name="password"
-        rules={[{ required: true, message: "Please input your password!" }]}
+        label="Description"
+        rules={[
+          {
+            required: false,
+            message: "Please input the description to video!",
+          },
+        ]}
       >
-        <Input.Password />
+        <TextArea
+          rows={10}
+          name="description"
+          onChange={handleChange}
+          value={data.description}
+        />
       </Form.Item>
 
       <Form.Item<FieldType>
-        name="remember"
-        valuePropName="checked"
-        label={null}
+        label="Url"
+        rules={[{ required: true, message: "Please input the url!" }]}
       >
-        <Checkbox>Remember me</Checkbox>
+        <Input name="url" onChange={handleChange} value={data.url} />
       </Form.Item>
 
-      <Form.Item label={null}>
+      {
+        <Form.Item<FieldType>
+          label="State Video"
+          rules={[{ required: true, message: "Please select the state!" }]}
+        >
+          <Switch
+            defaultChecked
+            onChange={onChangeState}
+            value={data.stateVideo}
+          />
+        </Form.Item>
+      }
+
+      <Form.Item>
         <Button type="primary" htmlType="submit">
           Submit
         </Button>
