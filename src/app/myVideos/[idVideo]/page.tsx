7a9c15ff0e3e898b5e4e1668e42b68c99b9fa3fd
migayalach <ayalachavezmiguel@gmail.com>
2025-03-07 23:25:51 -0400

    call Audit method ready

--- src/app/myVideos/[idVideo]/page.tsx
+++ src/app/myVideos/[idVideo]/page.tsx
@@ -1,26 +1,31 @@
 "use client";
 import React, { useEffect } from "react";
+import { useParams } from "next/navigation";
 import { useAppDispatch, useAppSelector } from "@/redux/hooks";
 import { getAuditVideo } from "@redux/features/audit.slice";
+import Table from "@/components/table/TableData";
 
 function page() {
   const dispatch = useAppDispatch();
+  const { idVideo }: { idVideo: string } = useParams();
   const selectUser = useAppSelector(({ sign }) => sign?.user);
+  const infoAudit = useAppSelector(({ audit }) => audit);
 
-  // useEffect(() => {
-  //     if (selectUser && videoInfo) {
-  //       dispatch(
-  //         getAuditVideo({
-  //           idVideo: videoInfo?.idVideo,
-  //           idUser: selectUser?.idUser,
-  //         })
-  //       );
-  //     }
-  //   }, []);
+  useEffect(() => {
+    if (selectUser) {
+      dispatch(
+        getAuditVideo({
+          idVideo,
+          idUser: selectUser?.idUser,
+        })
+      );
+    }
+  }, []);
 
   return (
     <div>
       <h1>Audit</h1>
+      <Table dataAudit={infoAudit?.results} />
     </div>
   );
 }
