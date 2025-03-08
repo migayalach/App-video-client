27a89af478cbead04b6f54b8e6e0e208feb8748b
migayalach <ayalachavezmiguel@gmail.com>
2025-03-07 23:29:52 -0400

    added methods accept delete and view audit

--- src/components/card/CardVideo.tsx
+++ src/components/card/CardVideo.tsx
@@ -1,7 +1,14 @@
-import React from "react";
-import { Card } from "antd";
+import React, { useState } from "react";
+import { Card, Button, Modal } from "antd";
 import { CardVideoProps } from "@/interfaces/video.interface";
 import Link from "next/link";
+import {
+  EditOutlined,
+  DeleteOutlined,
+  FileDoneOutlined,
+} from "@ant-design/icons";
+import { useAppDispatch, useAppSelector } from "@redux/hooks";
+import { deleleVideo } from "@/redux/features/myVideos.slice";
 
 const { Meta } = Card;
 
@@ -10,18 +17,78 @@ const CardVideo: React.FC<CardVideoProps> = ({
   nameVideo,
   image,
   average,
+  own,
 }) => {
+  const dispatch = useAppDispatch();
+  const selectUser = useAppSelector(({ sign }) => sign?.user);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleOk = () => {
+    setIsModalOpen(false);
+  };
+
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
+
+  const handleDelete = (idVideo: string) => {
+    if (selectUser) {
+      dispatch(deleleVideo({ idVideo, idUser: selectUser?.idUser }));
+      setIsModalOpen(false);
+    }
+  };
+
   return (
     <div>
-      <Link href={`/video/${idVideo}`}>
-        <Card
-          hoverable
-          style={{ width: 240, marginBottom: 16 }}
-          cover={<img alt={nameVideo} src={image} />}
+      <div>
+        <Link href={`/video/${idVideo}`}>
+          <Card
+            style={{ width: 300 }}
+            cover={<img alt={nameVideo} src={image} />}
+            actions={[
+              <EditOutlined key="edit" />,
+              <EditOutlined key="edit" />,
+              <DeleteOutlined key="delete" />,
+            ]}
+          >
+            <Meta title={nameVideo} />
+          </Card>
+        </Link>
+      </div>
+      {own && (
+        <div>
+          <Button type="primary" onClick={showModal}>
+            <DeleteOutlined />
+          </Button>
+          <Button color="cyan" variant="solid">
+            <Link href={`/myVideos/${idVideo}`}>
+              <FileDoneOutlined />
+            </Link>
+          </Button>
+        </div>
+      )}
+      {isModalOpen && (
+        <Modal
+          title="Delete video!"
+          open={isModalOpen}
+          onOk={handleOk}
+          onCancel={handleCancel}
         >
-          <Meta title={nameVideo} />
-        </Card>
-      </Link>
+          <div>
+            <p>Are you sure you want to delete this video?</p>
+            <Button type="primary" onClick={() => handleDelete(idVideo)}>
+              Yes
+            </Button>
+            <Button type="primary" danger>
+              No
+            </Button>
+          </div>
+        </Modal>
+      )}
     </div>
   );
 };
