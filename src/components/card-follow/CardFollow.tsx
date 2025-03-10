78548ec83fab27c49c594857a7a0cf8e6c010953
migayalach <ayalachavezmiguel@gmail.com>
2025-03-09 22:58:49 -0400

    render info to card

--- src/components/card-follow/CardFollow.tsx
+++ src/components/card-follow/CardFollow.tsx
@@ -1,10 +1,30 @@
 import React from "react";
+import { Card } from "antd";
 
-const CardFollow = () => {
+const { Meta } = Card;
+export interface DataInputCF {
+  idUser: string;
+  name: string;
+  email: string;
+  picture: string;
+}
+
+interface CardFollowProps extends DataInputCF {}
+
+const CardFollow: React.FC<CardFollowProps> = ({
+  idUser,
+  name,
+  email,
+  picture,
+}) => {
   return (
-    <div>
-      <h1>CardFollow</h1>
-    </div>
+    <Card
+      hoverable
+      style={{ width: 240 }}
+      cover={<img alt={name} src={picture} />}
+    >
+      <Meta title={name} description={email} />
+    </Card>
   );
 };
 
