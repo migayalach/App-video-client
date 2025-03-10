9be16213db03e46b3344d9fe94048954d98ecb88
migayalach <ayalachavezmiguel@gmail.com>
2025-03-09 22:57:24 -0400

    created form

--- src/components/form/form-filters/FormFilters.tsx
+++ src/components/form/form-filters/FormFilters.tsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React from "react";
+import type { FormProps } from "antd";
+import { Button, Form, Input } from "antd";
+import SelectOrder from "@/components/select/select-order/SelectOrder";
 
-function FormFilters() {
+type FieldType = {
+  username?: string;
+  password?: string;
+  remember?: string;
+};
+
+const onFinish: FormProps<FieldType>["onFinish"] = (values) => {
+  console.log("Success:", values);
+};
+
+const FormFilters: React.FC = () => {
   return (
-    <div>FormFilters</div>
-  )
-}
+    <Form
+      name="basic"
+      labelCol={{ span: 8 }}
+      wrapperCol={{ span: 16 }}
+      style={{ maxWidth: 600 }}
+      initialValues={{ remember: true }}
+      onFinish={onFinish}
+      autoComplete="off"
+    >
+      <Form.Item<FieldType> label="Order">
+        <SelectOrder />
+      </Form.Item>
 
-export default FormFilters
+      <Form.Item>
+        <Button type="primary" htmlType="submit">
+          Search
+        </Button>
+      </Form.Item>
+    </Form>
+  );
+};
+
+export default FormFilters;
