95e3f64ceff00448753c0579591a0bbaf747b475
migayalach <ayalachavezmiguel@gmail.com>
2025-03-09 22:56:30 -0400

    suscription ready

--- src/components/suscription/Suscription.tsx
+++ src/components/suscription/Suscription.tsx
@@ -1,14 +1,30 @@
 "use client";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { AppstoreAddOutlined } from "@ant-design/icons";
 import { Button } from "antd";
 import ModalMessage from "../modal/modalMessage/ModalMessage";
+import { useAppDispatch, useAppSelector } from "@/redux/hooks";
+import { addOrDeleteFollow } from "@/redux/features/follow.slice";
 
 function Suscription() {
+  const dispatch = useAppDispatch();
+  const infoUser = useAppSelector(({ sign }) => sign?.user);
+  const information = useAppSelector(({ videos }) => videos?.videoDetail);
+
   const [flag, setFlag] = useState(false);
 
   const handleAction = () => {
-    setFlag(true);
+    if (infoUser && infoUser && information) {
+      dispatch(
+        addOrDeleteFollow({
+          idUser: infoUser.idUser,
+          idCreador: information.idUser,
+          option: "Adding",
+        })
+      );
+    } else {
+      setFlag(true);
+    }
   };
 
   const handleActionOff = () => {
