9eabbd1ad9b8cf3018831adb6af02836206cde28
migayalach <ayalachavezmiguel@gmail.com>
2025-03-09 22:58:24 -0400

    render info to following

--- src/app/following/page.tsx
+++ src/app/following/page.tsx
@@ -1,9 +1,36 @@
-import React from 'react'
+"use client";
+import React, { useEffect } from "react";
+import { useAppDispatch, useAppSelector } from "@/redux/hooks";
+import { getAllFollowing } from "@/redux/features/follow.slice";
+import CardFollow from "@/components/card-follow/CardFollow";
 
 function page() {
+  const dispatch = useAppDispatch();
+  const infoUser = useAppSelector(({ sign }) => sign?.user);
+  const dataFollow = useAppSelector(({ follow }) => follow);
+
+  useEffect(() => {
+    if (infoUser) {
+      dispatch(getAllFollowing(infoUser?.idUser));
+    }
+  }, []);
+
   return (
-    <div>Following</div>
-  )
+    <>
+      {dataFollow?.results.map(({ idUser, name, email, picture }) => {
+        return (
+          <div key={idUser}>
+            <CardFollow
+              idUser={idUser}
+              name={name}
+              email={email}
+              picture={picture}
+            />
+          </div>
+        );
+      })}
+    </>
+  );
 }
 
-export default page
+export default page;
